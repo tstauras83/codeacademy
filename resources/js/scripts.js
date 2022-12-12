5fe3f6c0dfa8829ff03c9acf14bff667cfe0b778
Tauras <tstauras100@gmail.com>
2022-12-12 14:13:24 +0200

    JS initial commit

---
+++ resources/js/scripts.js
@@ -0,0 +1,19 @@
+//task 1
+let a = 1;
+console.log("Kintamojo a reikšmė: " + a);
+a++
+console.log("Kintamojo a reikšmė: " + a);
+
+
+//task 2
+let b = 'Tauras Semaska';
+let c = 'My name is '
+console.log(c + b);
+
+//task 3
+let year = prompt("Gimimo metai:") %10;
+let month = prompt("Gimimo menuo:") %10;
+let day = prompt("Gimimo diena:") %10;
+let amount = (year + month + day);
+console.log(`Jei paskutiniai gimimo datos skaičiai yra ${year}, ${month}, ${day}, tai suma bus lygi ${amount}.`);
+
