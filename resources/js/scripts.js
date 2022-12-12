10e0e5149e1283e91bb51867f4823e0e74e38c27
Tauras <tstauras100@gmail.com>
2022-12-12 14:48:48 +0200

    optimised code for task 3

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -16,4 +16,4 @@ let month = prompt("Gimimo menuo:") %10;
 let day = prompt("Gimimo diena:") %10;
 let amount = (year + month + day);
 console.log(`Jei paskutiniai gimimo datos skaičiai yra ${year}, ${month}, ${day}, tai suma bus lygi ${amount}.`);
-
+alert(`Jei paskutiniai gimimo datos skaičiai yra ${year}, ${month}, ${day}, tai suma bus lygi ${amount}.`);
