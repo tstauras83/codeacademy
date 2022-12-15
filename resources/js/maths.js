5c61b2d56ce75fcbb2719c60b33090b302d8788d
Tauras <tstauras100@gmail.com>
2022-12-15 11:48:35 +0200

    Maths tasks additional function

--- resources/js/maths.js
+++ resources/js/maths.js
@@ -10,36 +10,43 @@ document.getElementById('dats1').innerHTML +='<br> Adding together: ' + arr1 + '
 document.getElementById('dats2').innerHTML +='<br> Subtracting: ' + arr1 + ' from ' + arr2 + '<br> will result in: <br>';
 document.getElementById('dats3').innerHTML +='<br> Dividing: ' + arr1 + ' and ' + arr2 + '<br> will result in: <br>';
 document.getElementById('dats4').innerHTML +='<br> Multiplying: ' + arr1 + ' and ' + arr2 + '<br> will result in: <br>';
+
 function add(){
     for(let i1 = 0; i1 < arr1.length; i1++){
-        ats1=arr1[i1]+arr2[i1];
-        document.getElementById('dats1').innerHTML+='Adding: ' + arr1[i1]+' + '+arr2[i1] + ' = ' + ats1 + '<br>';
+        for (let j1 = 0; j1 < arr2.length; j1++){
+            ats1=arr1[i1]+arr2[j1];
+            document.getElementById('dats1').innerHTML+='Adding: ' + arr1[i1]+' + '+arr2[j1] + ' = ' + ats1 + '<br>';
+        }
     }
 }
 add();
 
-
-
 function subtract(){
-    for (let i2 = 0; i2 < arr1.length; i2++){
-        ats2 = arr1[i2] + arr2[i2];
-        document.getElementById('dats2').innerHTML+='Subtracting: ' + arr1[i2] + ' - ' + arr2[i2] + ' = ' + ats2 + '<br>';
+    for(let i2 = 0; i2 < arr1.length; i2++){
+        for (let j2 = 0; j2 < arr2.length; j2++){
+            ats2 = arr1[i2] - arr2[j2];
+            document.getElementById('dats2').innerHTML += 'Subtracting: ' + arr1[i2] + ' - ' + arr2[j2] + ' = ' + ats2 + '<br>';
+        }
     }
 }
 subtract();
 
-
 function divide(){
-    for (let i3 = 0; i3 < arr1.length; i3++){
-        ats3 = arr1[i3] / arr2[i3];
-        document.getElementById('dats3').innerHTML+='Subtracting: ' + arr1[i3] + ' / ' + arr2[i3] + ' = ' + ats3 + '<br>';
+    for(let i3 = 0; i3 < arr1.length; i3++){
+        for (let j3 = 0; j3 < arr2.length; j3++){
+            ats3 = arr1[i3] / arr2[j3];
+            document.getElementById('dats3').innerHTML += 'Dividing: ' + arr1[i3] + ' / ' + arr2[j3] + ' = ' + ats3 + '<br>';
+        }
     }
 }
 divide();
+
 function multiply(){
-    for (let i4 = 0; i4 < arr1.length; i4++){
-        ats4 = arr1[i4] * arr2[i4];
-        document.getElementById('dats4').innerHTML+='Subtracting: ' + arr1[i4] + ' * ' + arr2[i4] + ' = ' + ats4 + '<br>';
+    for(let i4 = 0; i4 < arr1.length; i4++){
+        for (let j4 = 0; j4 < arr2.length; j4++){
+            ats4 = arr1[i4] * arr2[j4];
+            document.getElementById('dats4').innerHTML += 'Multiplying: ' + arr1[i4] + ' * ' + arr2[j4] + ' = ' + ats4 + '<br>';
+        }
     }
 }
 multiply();
